50962d41608e3ca3648b9efb9130f17fea459e4c
EC2 Default User <ec2-user@ip-172-31-10-20.ap-southeast-2.compute.internal>
2014-03-21 11:40:49 +0000

    Auto copy generated rpm into bin dir

--- bin/makeRPM.sh
+++ bin/makeRPM.sh
@@ -8,7 +8,10 @@ cp hydra_basic_probe_init.d.sh ~/rpmbuild/SOURCES/hydra_basic_probe-2
 cp forever.sh ~/rpmbuild/SOURCES/hydra_basic_probe-2
 cp hydra_basic_probe.spec ~/rpmbuild/SPECS
 
-cd ~/rpmbuild/SOURCES/
+pushd ~/rpmbuild/SOURCES/
 tar czf hydra_basic_probe-2.0.tar.gz hydra_basic_probe-2/
 cd ~/rpmbuild 
 rpmbuild -ba SPECS/hydra_basic_probe.spec
+
+popd
+cp ~/rpmbuild/RPMS/noarch/hydra_basic_probe-2-0.noarch.rpm .

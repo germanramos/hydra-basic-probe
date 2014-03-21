8de1387175557e5a221c483d111db7d2e30c6a7c
EC2 Default User <ec2-user@ip-172-31-10-20.ap-southeast-2.compute.internal>
2014-03-21 13:00:47 +0000

    refactor

--- bin/makeRPM.sh
+++ bin/makeRPM.sh
@@ -6,12 +6,12 @@ mkdir ~/rpmbuild/SOURCES/hydra-basic-probe-2
 cp ../src/*  ~/rpmbuild/SOURCES/hydra-basic-probe-2
 cp hydra_basic_probe_init.d.sh ~/rpmbuild/SOURCES/hydra-basic-probe-2
 cp forever.sh ~/rpmbuild/SOURCES/hydra-basic-probe-2
-cp hydra_basic_probe.spec ~/rpmbuild/SPECS
+cp hydra-basic-probe.spec ~/rpmbuild/SPECS
 
 pushd ~/rpmbuild/SOURCES/
-tar czf hydra_basic_probe-2.0.tar.gz hydra-basic-probe-2/
+tar czf hydra-basic-probe-2.0.tar.gz hydra-basic-probe-2/
 cd ~/rpmbuild 
-rpmbuild -ba SPECS/hydra_basic_probe.spec
+rpmbuild -ba SPECS/hydra-basic-probe.spec
 
 popd
 cp ~/rpmbuild/RPMS/noarch/hydra-basic-probe-2-0.noarch.rpm .

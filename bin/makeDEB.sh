00a5e0c74bd1d11cddd309391d175aff1d505052
germanramos <german.ramos@gmail.com>
2014-03-21 13:53:40 +0100

    _ => -

---
+++ bin/makeDEB.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install alien
+sudo alien hydra_basic_probe-2-0.noarch.rpm
+	
--- bin/makeRPM.sh
+++ bin/makeRPM.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 sudo yum install rpm-build rpmdevtools
 rpmdev-setuptree
-mkdir ~/rpmbuild/SOURCES/hydra_basic_probe-2
 
-cp ../src/*  ~/rpmbuild/SOURCES/hydra_basic_probe-2
-cp hydra_basic_probe_init.d.sh ~/rpmbuild/SOURCES/hydra_basic_probe-2
-cp forever.sh ~/rpmbuild/SOURCES/hydra_basic_probe-2
+mkdir ~/rpmbuild/SOURCES/hydra-basic-probe-2
+cp ../src/*  ~/rpmbuild/SOURCES/hydra-basic-probe-2
+cp hydra_basic_probe_init.d.sh ~/rpmbuild/SOURCES/hydra-basic-probe-2
+cp forever.sh ~/rpmbuild/SOURCES/hydra-basic-probe-2
 cp hydra_basic_probe.spec ~/rpmbuild/SPECS
 
 pushd ~/rpmbuild/SOURCES/
-tar czf hydra_basic_probe-2.0.tar.gz hydra_basic_probe-2/
+tar czf hydra_basic_probe-2.0.tar.gz hydra-basic-probe-2/
 cd ~/rpmbuild 
 rpmbuild -ba SPECS/hydra_basic_probe.spec
 
 popd
-cp ~/rpmbuild/RPMS/noarch/hydra_basic_probe-2-0.noarch.rpm .
+cp ~/rpmbuild/RPMS/noarch/hydra-basic-probe-2-0.noarch.rpm .

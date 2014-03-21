acac63a1ac209a4003a118d99413eafd791ff593
EC2 Default User <ec2-user@ip-172-31-10-20.ap-southeast-2.compute.internal>
2014-03-21 10:57:46 +0000

    dd forever

--- bin/makeRPM.sh
+++ bin/makeRPM.sh
@@ -5,6 +5,7 @@ mkdir ~/rpmbuild/SOURCES/hydra_basic_probe-2
 
 cp ../src/*  ~/rpmbuild/SOURCES/hydra_basic_probe-2
 cp hydra_basic_probe_init.d.sh ~/rpmbuild/SOURCES/hydra_basic_probe-2
+cp forever.sh ~/rpmbuild/SOURCES/hydra_basic_probe-2
 cp hydra_basic_probe.spec ~/rpmbuild/SPECS
 
 cd ~/rpmbuild/SOURCES/
